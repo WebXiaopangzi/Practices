3c82ece54eeeccf3b0e546f0ae094763a5720d60
Xiaopangzi <webxiaopangzi@gmail.com>
2016-09-07 19:53:15 +0800

    task2_16
    delete call.  learn dataset.

--- task2_16/task2_16.js
+++ task2_16/task2_16.js
@@ -81,7 +81,7 @@ function delBtnHandle(city) {
 function init() {
 
 
-	 ！！！！！！！！还未搞懂！！！！！！！！！！！
+	 // ！！！！！！！！还未搞懂！！！！！！！！！！！
 
 
   // 在这下面给add-btn绑定一个点击事件，点击时触发addBtnHandle函数
@@ -89,7 +89,7 @@ function init() {
   // 想办法给aqi-table中的所有删除按钮绑定事件，触发delBtnHandle函数
   document.getElementById('aqi-table').addEventListener("click",function(e){
   	if(e.target.nodeName.toLowerCase() === "button"){
-  		delBtnHandle.call(null,event.target.dataset.city);
+  		delBtnHandle(event.target.dataset.city);
   	}
   })
 
